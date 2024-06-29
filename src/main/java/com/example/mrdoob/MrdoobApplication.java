9a8bb081d16bcbfaec0ac0120c15fdd459ddd2bf
jspinak <jspinak@gmail.com>
2024-06-29 10:08:50 +0200

    initial files

---
+++ src/main/java/com/example/mrdoob/MrdoobApplication.java
@@ -0,0 +1,13 @@
+package com.example.mrdoob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrdoobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrdoobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mrdoob/MrdoobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mrdoob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrdoobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
